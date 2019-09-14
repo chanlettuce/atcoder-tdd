f727760ce3582374b4161876445fd1b48aee555f
chanLettuce <lettuceandlettuce@gmail.com>
2019-09-14 20:33:13 +0900

    temp commit

--- src/atcoder/agc/037/problem.spec.ts
+++ src/atcoder/agc/037/problem.spec.ts
@@ -2,9 +2,24 @@ import { solve as a } from './problem-a';
 
 type Solver = (lines: string[]) => string;
 type Params = Array<[string, string]>;
+type Problems = Array<{
+  level: string;
+  solver: Solver;
+  json: string;
+  customParams?: Params;
+}>;
+
+const contestName = '';
+const problems: Problems = [
+  {
+    level: 'a',
+    solver: a,
+    json: `[["aabbaa","4"],["aaaccacabaababc","12"]]`,
+  },
+];
 
 const runTest = (solver: Solver, params: Params, customParams: Params = []) => {
-  const test = param => {
+  const test = (param: [string, string]) => {
     const [i, o] = param;
     expect(solver(i.split('\n'))).toBe(o);
   };
@@ -18,16 +33,10 @@ const runTest = (solver: Solver, params: Params, customParams: Params = []) => {
 
 jest.setTimeout(1000 * 2);
 
-describe('ABC134', () => {
-  describe('F', () => {});
-  describe('E', () => {});
-  describe('D', () => {});
-  describe('C', () => {});
-  describe('B', () => {});
-  describe('A', () => {
-    const json = `[["aabbaa","4"],["aaaccacabaababc","12"]]`;
-    const params = JSON.parse(json);
-    const customParams = [];
-    runTest(a, params, customParams);
+describe(contestName, () => {
+  problems.forEach(({ level, solver, json, customParams }) => {
+    describe(level, () => {
+      runTest(solver, JSON.parse(json), customParams);
+    });
   });
 });
